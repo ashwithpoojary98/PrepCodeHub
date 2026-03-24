3b615c013d54eb646f8f9d17f1a0c86e2ce30678
ashwithpoojary98 <ashwithpoojary98@gmail.com>
2026-03-24 21:21:05 +0530

    feat: added new code

--- Java/DSA/src/main/java/io/github/ashwithpoojary98/basic/PriorityQueueTest.java
+++ Java/DSA/src/main/java/io/github/ashwithpoojary98/basic/PriorityQueueTest.java
@@ -5,7 +5,7 @@ import java.util.PriorityQueue;
 public class PriorityQueueTest {
     
     public static void main(String[] args){
-        PriorityQueue<String> queue=new PriorityQueue();
+        PriorityQueue<String> queue=new PriorityQueue<> ();
         queue.add("1");
          queue.add("3");
         queue.add("2");
